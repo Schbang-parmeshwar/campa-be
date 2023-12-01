d39fb5a8712269d3807825a93e7ac844ac5d6f44
parmeshwar rathod <parmeshwar.rathod@schbang.com>
2023-12-01 15:59:42 +0530

    chore: updated origins

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,6 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000', 'https://campa-cola.vercel.app', 'https://hdz0xl9q-1337.inc1.devtunnels.ms/', 'https://hdz0xl9q-1337.inc1.devtunnels.ms/api/layout?populate=deep','http://18.225.31.186']
-    }
-  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
@@ -45,7 +39,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', "http://localhost:1337"],
+      origin: ['http://localhost:3000', 'https://campa-cola.vercel.app', 'https://hdz0xl9q-1337.inc1.devtunnels.ms/', 'https://hdz0xl9q-1337.inc1.devtunnels.ms/api/layout?populate=deep','http://18.225.31.186'],
       // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       // headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
